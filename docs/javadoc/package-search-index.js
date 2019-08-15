d5b604ce55e931665cd69f26407a6274b61277eb
Hogwai <lilian.wernert@gmail.com>
2019-08-15 15:34:55 +0200

    Suppression JavaDoc dans docs/
    Suppression de la JavaDoc dans le dossier docs/

--- docs/javadoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.hogwai.p3"},{"l":"com.hogwai.p3.joueur"},{"l":"com.hogwai.p3.mode"},{"l":"com.hogwai.p3.runtime"}]
